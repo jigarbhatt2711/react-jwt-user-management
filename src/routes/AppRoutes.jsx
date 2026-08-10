8326901f37b740e2f82fc3e7f285b1f10abbb560
Krunal Babaria <krunalb.07@gmail.com>
2026-08-10 17:57:18 +0530

    Lazy loading consept add

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,25 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
-import Dashboard from "../pages/Dashboard";
-import AboutUs from "../pages/AboutUs";
-import Tc from "../pages/TC";
 import MainLayout from "../components/Layout/mainLayout";
 import ProtectedRoute from "./ProtectedRoute";
 import UserLayout from "../components/Layout/UserLayout";
 import HomeRedirect from "./HomeRedirect";
-import Users from "../pages/Users";
+import { lazy } from "react";
 
 export default function AppRoutes() {
+  const Users = lazy(() => import('../pages/Users'));
+  const Dashboard = lazy(() => import('../pages/Dashboard'));
+  const AboutUs = lazy(() => import('../pages/AboutUs'));
+  const Tc = lazy(() => import('../pages/TC'));
   return (
     <Routes>
       <Route element={<MainLayout />}>
-         <Route path="/" element={<HomeRedirect />} />
+        <Route path="/" element={<HomeRedirect />} />
         <Route path="/login" element={<Login />} />
-            <Route path="/about_us" element={<AboutUs />} />
-            <Route path="/tc" element={<Tc />} />
+        <Route path="/about_us" element={<AboutUs />} />
+        <Route path="/tc" element={<Tc />} />
       </Route>
 
-      <Route element={<UserLayout />}>  
+      <Route element={<UserLayout />}>
         <Route element={<ProtectedRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/users" element={<Users />} />
