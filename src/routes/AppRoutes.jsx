040d2e307e8f91250a423168ef0f237f32bbd747
Krunal Babaria <krunalb.07@gmail.com>
2026-08-10 18:03:54 +0530

    Suspend user for lazyloading page display till display 'loading..' text on the screen

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,7 +4,17 @@ import MainLayout from "../components/Layout/mainLayout";
 import ProtectedRoute from "./ProtectedRoute";
 import UserLayout from "../components/Layout/UserLayout";
 import HomeRedirect from "./HomeRedirect";
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
+
+const Loading = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-lg font-semibold text-gray-600">
+        Loading...
+      </div>
+    </div>
+  );
+};
 
 export default function AppRoutes() {
   const Users = lazy(() => import('../pages/Users'));
@@ -12,21 +22,22 @@ export default function AppRoutes() {
   const AboutUs = lazy(() => import('../pages/AboutUs'));
   const Tc = lazy(() => import('../pages/TC'));
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<HomeRedirect />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/about_us" element={<AboutUs />} />
-        <Route path="/tc" element={<Tc />} />
-      </Route>
-
-      <Route element={<UserLayout />}>
-        <Route element={<ProtectedRoute />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/users" element={<Users />} />
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomeRedirect />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about_us" element={<AboutUs />} />
+          <Route path="/tc" element={<Tc />} />
         </Route>
-      </Route>
-    </Routes>
 
+        <Route element={<UserLayout />}>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/users" element={<Users />} />
+          </Route>
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
